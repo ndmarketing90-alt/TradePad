7651b51d03f3e88758e871aabd87bc387f0c6cc2
ndmarketing90-alt <ndmarketing90@gmail.com>
2026-05-24 12:15:55 +0100

    Update application.py

--- application.py
+++ application.py
@@ -18,7 +18,7 @@ else:
 
     # --- CONNECT TO DATABASE ---
     # We use Streamlit's built-in cloud database connection
-   db = st.connection("st_firestore", type="st_firestore")
+    db = st.connection("st_firestore", type="st_firestore")
 
     # Define where this user's trades are stored in the cloud
     user_ref = db.collection("traders").document(username)
